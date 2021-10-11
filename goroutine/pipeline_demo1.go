60f54fffa2ff09f2ba7e2392c5100932faba047f
Jiaqi Yin <jiaqi.yin.io@gmail.com>
2021-10-12 00:12:24 +1100

    Add goroutine pipeline demos.

---
+++ goroutine/pipeline_demo1.go
@@ -0,0 +1,34 @@
+// Squaring numbers
+package main
+
+import "fmt"
+
+// First stage
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+// Second stage
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	for n := range sq(gen(1, 2, 3)) {
+		fmt.Println(n)
+	}
+}
